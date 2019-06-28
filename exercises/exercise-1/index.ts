6f1146e482faa5b5d4af8361b6d12ba6e3977d35
Keith Bingman <keith@keithbingman.com>
2019-06-28 09:20:39 -0700

    adding npm tasks

---
+++ exercises/exercise-1/index.ts
@@ -0,0 +1,12 @@
+import { SearchPage } from './pages/SearchPage';
+
+const main = async () => {
+  const searchPage = new SearchPage('https://www.google.com/');
+  await searchPage.goToSearchPage();
+  await searchPage.search('number six battlestar galactica');
+  const results = await searchPage.getSearchResults();
+  await searchPage.close();
+  console.log(results);
+};
+
+main(); 
--- exercises/exercise-1/pages/SearchPage.ts
+++ exercises/exercise-1/pages/SearchPage.ts
@@ -1,4 +1,4 @@
-import { BasePage } from './BasePage';
+import { BasePage } from "./BasePage";
 
 export class SearchPage extends BasePage {
   private _url: string;
@@ -6,14 +6,14 @@ export class SearchPage extends BasePage {
   private readonly SELECTORS = {
     SEARCH_FIELD: '[name="q"]',
     SEARCH_BTN: '[name="btnK"]',
-    SEARCH_CONTAINER: '#search',
-    SEARCH_RESULT: '#search .g',
-    RESULT_TITLE: '.LC20lb',
-    RESULT_LINK: '.r a',
+    SEARCH_CONTAINER: "#search",
+    SEARCH_RESULT: "#search .g",
+    RESULT_TITLE: ".LC20lb",
+    RESULT_LINK: ".r a"
   };
 
-  constructor (url: string) {
-    super()
+  constructor(url: string) {
+    super();
     this._url = url;
   }
 
@@ -26,24 +26,23 @@ export class SearchPage extends BasePage {
     await this.page.type(this.SELECTORS.SEARCH_FIELD, query);
     await this.page.$eval(this.SELECTORS.SEARCH_FIELD, (e: any) => e.blur());
     await this.page.waitFor(this.SELECTORS.SEARCH_BTN);
+    await this.page.waitFor(1000);
     await this.page.click(this.SELECTORS.SEARCH_BTN);
   }
 
   public async getSearchResults(): Promise<any[]> {
-    const { SEARCH_CONTAINER, SEARCH_RESULT, RESULT_TITLE } = this.SELECTORS;
+    await this.page.waitFor(1000);
 
-    await this.page.waitFor(2000);
-
-    return await this.page.$$eval(SEARCH_RESULT, elements => {
+    return await this.page.$$eval(this.SELECTORS.SEARCH_RESULT, (elements, selectors) => {
       return elements.map(el => {
-        const title: HTMLElement = el.querySelector('.LC20lb');
-        const link: HTMLAnchorElement = el.querySelector('.r a');
-
+        const title: HTMLElement = el.querySelector(selectors.RESULT_TITLE);
+        const link: HTMLAnchorElement = el.querySelector(selectors.RESULT_LINK);
+  
         return {
-          text: title.textContent,
+          title: title.textContent,
           url: link.href
-        }
+        };
       });
-    });
+    }, this.SELECTORS);
   }
 }
